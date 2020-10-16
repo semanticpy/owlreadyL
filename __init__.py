4e67884dddad4cd2b3dc65076f1481bd96943072
jibalamy <jibalamy@free.fr>
2020-10-16 16:50:32 +0200

    update version number

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-VERSION = "0.24"
+VERSION = "0.25"
 
 JAVA_EXE = "java"
 
