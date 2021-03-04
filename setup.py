8225111e6628cbe2b744bf0939e08ba6da19157a
Carsten Knoll <carsten.knoll@posteo.de>
2021-03-04 17:54:09 +0100

    fix a problem with absoulte paths ending up in SOURCES.txt

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@
 
 import os, os.path, sys, glob
 
-# get the absolute path of the directory of this file
-HERE = os.path.dirname(os.path.abspath(sys.modules.get(__name__).__file__))
+# get a canonical representation of relative path of the directory of this file
+HERE = os.path.relpath(os.path.dirname(os.path.abspath(sys.modules.get(__name__).__file__)))
 
 if len(sys.argv) <= 1: sys.argv.append("install")
 
