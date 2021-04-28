0d09616759409038ad3bc58b4044b13280ab014e
jibalamy <jibalamy@free.fr>
2021-04-28 14:25:05 +0200

    Improve bench; add missing files

---
+++ test/bench_berlin_sparql.py
@@ -0,0 +1,62 @@
+
+# Run with:
+# gunicorn -b 127.0.0.1:5000 --preload -w 5 --worker-class=gevent owlready2.test.bench_berlin_sparql:app
+
+# Or:
+# uwsgi --http 127.0.0.1:5000 --plugin python -b 128000 -p 8 --module owlready2.test.bench_berlin_sparql:app > /dev/null 2>&1
+# uwsgi --http 127.0.0.1:5000 -s /tmp/yourapplication.sock --plugin python -b 128000 -p 8 --manage-script-name --mount /=owlready2.test.bench_berlin_sparql:app > /dev/null 2>&1
+
+# Then run Berlin SPARQL benchmark with:
+# cd /home/jiba/telechargements/bsbmtools-0.2
+# ./testdriver -w 1 -runs 10 -mt 5 http://localhost:5000/sparql
+
+import sys, os, time, flask
+
+from owlready2 import *
+from owlready2.sparql.endpoint import *
+
+QUADSTORE_ON_DISK = True
+
+if QUADSTORE_ON_DISK:
+  try: os.unlink("/tmp/t.sqlite3")
+  except: pass
+  try: os.unlink("/tmp/t.sqlite3-journal")
+  except: pass
+  world = World()
+  world.set_backend(filename = "/tmp/t.sqlite3") #, exclusive = False) # Exclusive is not needed here, since it is read ontly
+  
+  class USD(float):
+    def __new__(self, value): return float.__new__(self, value)
+  def parser(s):   return USD(s)
+  def unparser(x): return str(x)
+  declare_datatype(USD, "http://www4.wiwiss.fu-berlin.de/bizer/bsbm/v01/vocabulary/USD", parser, unparser)
+  
+  onto = world.get_ontology("/home/jiba/telechargements/bsbmtools-0.2/dataset.nt").load()
+  world.save()
+  world.close()
+  
+  default_world.set_backend(filename = "/tmp/t.sqlite3", read_only = True, exclusive = True)
+  #default_world.set_backend(filename = "/tmp/t.sqlite3", exclusive = True)
+  
+  
+app = flask.Flask("OwlreadyBench")
+endpoint = EndPoint(default_world)
+
+#def endpoint():
+  #for i in range(2000000): pass
+  #time.sleep(0.05)
+  #return """<?xml version="1.0"?><sparql xmlns="http://www.w3.org/2005/sparql-results#"></sparql>"""
+  
+app.route("/sparql", methods = ["GET"])(endpoint)
+
+
+@app.route("/optimize", methods = ["GET"])
+def optimize():
+  default_world.graph.execute("""PRAGMA OPTIMIZE""")
+  default_world.graph.execute("""PRAGMA VACUUM""")
+  default_world.save()
+  return "ok"
+  
+if __name__ == "__main__":
+  import bjoern
+  bjoern.run(app, "127.0.0.1", 5000)
---
+++ test/bench_sparql.py
@@ -0,0 +1,99 @@
+# python ./owlready2/test/bench_sparql.py
+
+# You need to run the benchmark twice, because the first time, GO is not loaded in memory, which favor the second
+# tested SPARQL engine (which is RDFlib)
+
+import sys, time, rdflib
+
+import owlready2
+from owlready2 import *
+
+default_world.set_backend(filename = "/home/jiba/tmp/go.sqlite3", exclusive = True) #, profiling = True) 
+go  = get_ontology ("http://purl.obolibrary.org/obo/go.owl").load()
+obo = get_namespace("http://purl.obolibrary.org/obo/")
+default_world.save()
+
+PLANS = ""
+
+def sparql_owlready(sparql):
+  #global PLANS
+  #q = default_world.prepare_sparql(sparql)
+  #plan = list(default_world.graph.execute("""EXPLAIN QUERY PLAN %s""" % q.sql))
+  #plan = "\n".join(" ".join(str(j) for j in l) for l in plan)
+  #PLANS += plan + "\n\n"
+  
+  #print(default_world.prepare_sparql(sparql).sql)
+  
+  t0 = time.time()
+  r = list(default_world.sparql(sparql))
+  t = time.time() - t0
+  
+  print("OWLREADY: %s s" % t)
+  return r, t
+
+def sparql_rdflib(sparql):
+  t0 = time.time()
+  r = list(default_world.sparql_query(sparql))
+  t = time.time() - t0
+  print("RDFLIB:   %s s" % t)
+  
+  return r, t
+
+T_OWLREADY = T_RDFLIB = 0.0
+def sparql(sparql):
+  global T_OWLREADY, T_RDFLIB
+
+  r1, t1 = sparql_owlready(sparql)
+  r2, t2 = sparql_rdflib  (sparql)
+  T_OWLREADY += t1
+  T_RDFLIB   += t2
+  print("Ratio: %s" % (t2 / t1))
+  if len(r1) != len(r2):
+    print("OWLREADY: %s" % r1)
+    print("RDFLIB: %s" % r2)
+    print("OWLREADY: %s results" % len(r1))
+    print("RDFLIB: %s results" % len(r2))
+    assert False
+  else:
+    print("%s results" % len(r1))
+  print()
+
+# Loads parsers
+list(default_world.sparql      ("""SELECT  ?x  { ?x rdfs:label "xxx" . }"""))
+list(default_world.sparql_query("""SELECT  ?x  { ?x rdfs:label "xxx" . }"""))
+print()
+
+
+
+sparql("""SELECT ?l  { ?x rdfs:subClassOf* <http://purl.obolibrary.org/obo/GO_0005575> . ?x rdfs:label ?l }""")
+
+sparql("""SELECT ?l  { ?x a/rdfs:subClassOf* <http://purl.obolibrary.org/obo/GO_0005575> . ?x rdfs:label ?l }""")
+
+sparql("""SELECT (STR(?x) AS ?i)  { ?x rdfs:subClassOf* <http://purl.obolibrary.org/obo/GO_0005575> . FILTER EXISTS { ?x rdfs:label ?l } }""")
+
+sparql("""SELECT (STR(?x) AS ?i)  { ?x rdfs:subClassOf* <http://purl.obolibrary.org/obo/GO_0005575> . FILTER NOT EXISTS { ?x rdfs:label ?l } }""")
+
+sparql("""SELECT (STR(?x) AS ?i)  { ?x a <http://www.w3.org/2002/07/owl#Class> . ?x rdfs:label ?l. FILTER (STRSTARTS(?l, "A")) }""")
+
+sparql("""SELECT ?l  { { ?x rdfs:subClassOf* <http://purl.obolibrary.org/obo/GO_0005575> . } UNION { ?x rdfs:subClassOf* <http://purl.obolibrary.org/obo/GO_0008150> . } ?x rdfs:label ?l }""")
+
+sparql("""SELECT (STR(?x) AS ?i)  { ?x a <http://www.w3.org/2002/07/owl#Class> . FILTER (ISIRI(?x)) FILTER NOT EXISTS { ?x rdfs:label ?l } }""")
+
+sparql("""SELECT (COUNT(?x) AS ?i)  { ?x a <http://www.w3.org/2002/07/owl#Class> . }""")
+
+sparql("""SELECT (STR(?x) AS ?i)  { ?x rdfs:subClassOf* <http://purl.obolibrary.org/obo/GO_0008150> . ?x rdfs:subClassOf* <http://purl.obolibrary.org/obo/GO_0005575> }""")
+
+
+print()
+print("# TOTAL:")
+print("# OWLREADY: %s s" % T_OWLREADY)
+print("# RDFLIB:   %s s" % T_RDFLIB)
+print("# Mean ratio: %s" % (T_RDFLIB / T_OWLREADY))
+
+# TOTAL:
+# OWLREADY: 0.4174220561981201 s
+# RDFLIB:   42.52021360397339 s
+# Mean ratio: 101.863840141193
+
+
+#open("/tmp/plans.txt", "w").write(PLANS)
